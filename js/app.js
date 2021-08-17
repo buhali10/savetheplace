413c340dcca2e1876446df0310034c7e4c273963
buhali10 <buhali10@gmail.com>
2021-08-17 13:23:05 +0200

    Stand 17.8

--- js/app.js
+++ js/app.js
@@ -1,13 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('service worker registered', reg))
-      .catch(err => console.log('service worker not registered', err));
-  }
-
-  navigator.serviceWorker.getRegistrations()
-  .then(function(registrations) { 
-      for(let registration of registrations) 
-      { registration.unregister() 
-    } 
-})
-
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('service worker registered'))
+    .catch(err => console.log('service worker not registered', err));
+}
--- js/db.js
+++ js/db.js
@@ -1,6 +1,6 @@
 // enable offline data
 db.enablePersistence()
-  .catch(function (err) {
+  .catch(function(err) {
     if (err.code == 'failed-precondition') {
       // probably multible tabs open at once
       console.log('persistance failed');
@@ -13,112 +13,38 @@ db.enablePersistence()
 // real-time listener
 db.collection('places').onSnapshot(snapshot => {
   snapshot.docChanges().forEach(change => {
-    if (change.type === 'added') {
+    if(change.type === 'added'){
       renderplace(change.doc.data(), change.doc.id);
     }
-    if (change.type === 'removed') {
+    if(change.type === 'removed'){
       removeplace(change.doc.id);
     }
   });
 });
 
-(function () {
-  var locatorSection = document.getElementById("locator-input-section")
-
-  function init() {
-      var locatorButton = document.getElementById("locator-button");
-      locatorButton.addEventListener("click", locatorButtonPressed)
-  }
-
-  function locatorButtonPressed() {
-      locatorSection.classList.add("loading")
-
-      navigator.geolocation.getCurrentPosition(function (position) {
-              getUserAddressBy(position.coords.latitude, position.coords.longitude)
-          },
-          function (error) {
-              locatorSection.classList.remove("loading")
-              alert("The Locator was denied :( Please add your address manually")
-          })
-  }
-
-  function getUserAddressBy(lat, long) {
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
-          if (this.readyState == 4 && this.status == 200) {
-              var address = JSON.parse(this.responseText)
-              setAddressToInputField(address.results[0].formatted_address)
-          }
-      };
-      xhttp.open("GET", "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + "&key=****", true);
-      xhttp.send();
-  }
-
-  function setAddressToInputField(address) {
-      var input = document.getElementById("autocomplete");
-      input.value = address
-      locatorSection.classList.remove("loading")
-  }
-
-  init()
-
-})();
-
 // add new place
+const form = document.querySelector('form');
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  
+  const place = {
+    name: form.title.value,
+    city: form.city.value
+  };
 
-function createForm(position) {
-  const form = document.querySelector('form');
-  const longitude = position.coords.longitude;
-  const latitud = position.coords.latitude;
+  db.collection('places').add(place)
+    .catch(err => console.log(err));
 
-  form.addEventListener('submit', evt => {
-    evt.preventDefault();
-
-    const place = {
-      name: form.name.value,
-      city: form.city.value,
-      latitude: latitude,
-      longitude: longitude
-    };
-
-    db.collection('places').add(place)
-      .catch(err => console.log(err));
-
-    form.name.value = '';
-    form.city.value = '';
-    console.log(place.name + ' in ' + place.name + ' is added to the gallery')
-  });
-
-};
-
+  form.title.value = '';
+  form.city.value = '';
+});
 
 // remove a place
 const placeContainer = document.querySelector('.places');
 placeContainer.addEventListener('click', evt => {
-  if (evt.target.tagName === 'I') {
+  if(evt.target.tagName === 'I'){
     const id = evt.target.getAttribute('data-id');
     //console.log(id);
     db.collection('places').doc(id).delete();
-    console.log('Object' + id + 'is deleted')
   }
 })
-
-
-//function that gets the location and returns it
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    console.log("Geo Location not supported by browser");
-  }
-}
-
-
-//function that retrieves the position
-function showPosition(position) {
-  var location = {
-    longitude: position.coords.longitude,
-    latitude: position.coords.latitude
-  }
-  console.log(location)
-}
